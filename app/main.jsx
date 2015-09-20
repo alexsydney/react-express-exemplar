28806d61e690cdc69d71f5a6371fad0c5e2c53a1
Edward Smith <edward_smith26@Edwards-MacBook-Pro.local>
2015-09-20 02:21:16 +0100

    Initial commit, completed section two: scaffolding the app

---
+++ app/main.jsx
@@ -0,0 +1,7 @@
+var React = require('react/addons');
+
+console.log('Hello from jsx!')
+
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+React.render(<GroceryItemList />, app);
