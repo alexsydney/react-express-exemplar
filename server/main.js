0799bb573d5e0f388aa24af7cf844b89afa10042
ezmionline <edward@ezmi.co.uk>
2015-10-24 08:47:05 +0100

    added body parser via npm

--- server/main.js
+++ server/main.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = new express();
 
+var parser = require('body-parser');
+
 app.get('/', function(req, res){
   res.render('./../app/index.ejs', {});
 })
@@ -9,4 +11,7 @@ app.get('/', function(req, res){
 .use('/bower_components',express.static(__dirname + '/../bower_components'))
 .listen(7777);
 
-require("./routes/items.js")(app);
+app.use(parser.json());
+app.use(parser.urlencoded());
+
+require('./routes/items.js')(app);
