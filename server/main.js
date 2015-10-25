1c6aa65096222d079db9f2a1722d1680502c61a7
ezmionline <edward@ezmi.co.uk>
2015-10-25 20:53:20 +0000

    partial code commit for isomorphic features, pending due to .jsx require error

--- server/main.js
+++ server/main.js
@@ -4,10 +4,26 @@ var app = new express();
 
 var parser = require('body-parser');
 
+var React = require('react/addons');
+
+var GroceryItem = require('./models/GroceryItem.js');
+
+require('babel/register');
 require('./database.js');
 
 app.get('/', function(req, res){
   res.render('./../app/index.ejs', {});
+  /*var application = React.createFactory(require('./../app/components/GroceryItemList.jsx'));
+
+  GroceryItem.find(function(error, doc){
+    var generated = React.renderToString(application({
+      items: doc
+    }));
+
+    res.render('./../app/index.jsx', {
+      reactOutput: generated
+    })
+  });*/
 })
 .use(express.static(__dirname + '/../.tmp'))
 .use('/bower_components',express.static(__dirname + '/../bower_components'))
